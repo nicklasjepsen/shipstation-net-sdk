a876247e2a15d007c8bae6465cdf4d1c88464732
Nicklas Møller Jepsen <nicklas.m.jepsen@gmail.com>
2022-06-13 22:37:36 +0200

    added sample console app

---
+++ ShipStation.NetSdk/ShipStation.NetSdk.ConsoleExample/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ShipStation.NetSdk.ConsoleExample
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
