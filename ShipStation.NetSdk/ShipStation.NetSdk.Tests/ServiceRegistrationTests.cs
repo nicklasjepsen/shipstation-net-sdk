8f9c0b4ce7dad71be04fa877a93a2476b97ac69d
Nicklas Møller Jepsen <nicklas.m.jepsen@gmail.com>
2022-06-14 06:49:26 +0200

    added unit test for service registration

---
+++ ShipStation.NetSdk/ShipStation.NetSdk.Tests/ServiceRegistrationTests.cs
@@ -0,0 +1,48 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ShipStation.NetSdk.Tests
+{
+    public class ServiceRegistrationTests
+    {
+        [Fact]
+        public void ServiceCollectionRegistration_Options_Null_Exception()
+        {
+            var services = new ServiceCollection();
+            Assert.Throws<ArgumentNullException>(() => services.AddShipStation(null));
+        }
+
+        [Theory]
+        [InlineData("", "", "")]
+        [InlineData("https://valid.url/", "", "")]
+        [InlineData("https://valid.url/", "validapikey", "")]
+        public void ServiceCollectionRegistration_Options_Invalid_Values(string url, string apiKey, string apiSecret)
+        {
+            var services = new ServiceCollection();
+            Assert.Throws<ArgumentException>(() => services.AddShipStation(new ShipStationOptions(url, apiKey, apiSecret)));
+        }
+
+        [Theory]
+        [InlineData("https://valid.url/", "validapikey", "validapisecret")]
+        public void ServiceCollectionRegistration_Options_Valid_Values(string url, string apiKey, string apiSecret)
+        {
+            var services = new ServiceCollection();
+            services.AddShipStation(new ShipStationOptions(url, apiKey, apiSecret));
+        }
+
+        [Theory]
+        [InlineData("https://valid.url/", "validapikey", "validapisecret")]
+        public void ServiceCollectionRegistration_Options_Assigned_In_Service(string url, string apiKey, string apiSecret)
+        {
+            var services = new ServiceCollection();
+            services.AddShipStation(new ShipStationOptions(url, apiKey, apiSecret));
+
+            var provider = services.BuildServiceProvider();
+            var service = provider.GetRequiredService<IHttpClientFactory>();
+            Assert.NotNull(service);
+            var httpClient = service.CreateClient(nameof(ShipStationService));
+            Assert.NotNull(httpClient);
+            Assert.Equal(url, httpClient.BaseAddress?.ToString());
+            Assert.Equal($"{apiKey}:{apiSecret}".Base64Encode(), httpClient.DefaultRequestHeaders.Authorization?.Parameter);
+        }
+    }
+}
--- ShipStation.NetSdk/ShipStation.NetSdk/ServiceRegistrationExtensions.cs
+++ ShipStation.NetSdk/ShipStation.NetSdk/ServiceRegistrationExtensions.cs
@@ -7,6 +7,15 @@ namespace ShipStation.NetSdk
     {
         public static void AddShipStation(this IServiceCollection services, ShipStationOptions options)
         {
+            if(options == null)
+                throw new ArgumentNullException(nameof(options));
+            if(string.IsNullOrEmpty(options.BaseUrl))
+                throw new ArgumentException("BaseUrl must be a valid url.");
+            if (string.IsNullOrEmpty(options.ApiKey))
+                throw new ArgumentException("ApiKey must be set.");
+            if (string.IsNullOrEmpty(options.ApiSecret))
+                throw new ArgumentException("ApiSecret must be set.");
+
             services.AddHttpClient<ShipStationService>(client =>
             {
                 client.BaseAddress = new Uri(options.BaseUrl);
